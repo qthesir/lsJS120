caf297aa856cbe14fb66f6d30d1d07383d47105f
q_the_sir <quinlan.anderson@gmail.com>
2025-10-27 23:28:23 -0400

    wip

--- lesson1/ooRPS.js
+++ lesson1/ooRPS.js
@@ -68,33 +68,149 @@ kind of hard to describe!
 
 */
 
+const rlsync = require("readline-sync");
+
 function createPlayer() {
   return {
-    // possible state: player name?
-    // possible state: player current move?
-    // possible state: player or computer? 
+    availableMoves: createMove(),
+    currentMove: null,
+    score: 0,
+  };
+}
+
+function createComputer() {
+  let playerObject = createPlayer();
+
+  let computerObject = {
 
     choose() {
+      let randomIndex = Math.floor(
+        Math.random() * this.availableMoves.moves.length
+      );
+      this.currentMove = this.availableMoves.moves[randomIndex];
+    },
+  };
 
-    }
-  }
+  return Object.assign(playerObject, computerObject);
+}
+
+function createHuman() {
+  let playerObject = createPlayer();
+
+  let humanObject = {
+
+    choose() {
+      let choice;
+      while (true) {
+        console.log(
+          "Choose one of the following options: rock, paper, or scissors: "
+        );
+        choice = rlsync.question();
+        if (this.availableMoves.moves.includes(choice)) break;
+        console.log("Sorry, invalid choice");
+      }
+
+      this.currentMove = choice;
+    },
+  };
+
+  return Object.assign(playerObject, humanObject);
 }
 
 function createMove() {
   return {
-    // type of move (rock, paper, scissors)
+    moves: ["rock", "paper", "scissors"],
+  };
+}
+
+let compare = function (move1, move2) {
+  if (move1 === "rock" && move2 === "scissors") {
+    return move1;
+  } else if (move1 === "scissors" && move2 === "paper") {
+    return move1;
+  } else if (move1 === "paper" && move2 === "rock") {
+    return move1;
+  } else if (move2 === "rock" && move1 === "scissors") {
+    return move2;
+  } else if (move2 === "scissors" && move1 === "paper") {
+    return move2;
+  } else if (move2 === "paper" && move1 === "rock") {
+    return move2;
+  } else {
+    return "tie";
   }
-}
+};
 
-function createRule() {
-  return{
-    // do rules need state? Might just be a static object
-  }
-}
+const RPSGame = {
+  human: createHuman(),
+  computer: createComputer(),
 
-let compare = function(move1, move2) {
+  displayWelcomeMessage() {
+    console.log("Welcome to Rock, Paper, Scissors!");
+  },
 
-}
+  displayGoodbyeMessage() {
+    console.log("Thanks for playing Rock, Paper, Scissors. Goodbye!");
+  },
 
+  displayWinner() {
+    console.log(`You chose ${this.human.currentMove}`);
+    console.log(`Computer chose ${this.computer.currentMove}`);
+    let winner = compare(this.human.currentMove, this.computer.currentMove);
+    if (winner === "tie") {
+      console.log("Its a tie!");
+    } else {
+      console.log(
+        this.human.currentMove === winner ? "You win!" : "Computer wins!"
+      );
+    }
+  },
 
+  displayScore() {},
 
+  pickGrandWinner() {},
+
+  displayGrandWinner() {},
+
+  playAgain() {
+    console.log("Would you like to play again? (y/n)");
+    let answer = rlsync.question();
+    return answer.toLowerCase()[0] === "y";
+  },
+
+  play() {
+    this.displayWelcomeMessage();
+    while (true) {
+      while (true) {
+        this.human.choose();
+        this.computer.choose();
+        this.displayWinner();
+        this.displayScore();
+      }
+      this.displayGrandWinner();
+      if (!this.playAgain()) break;
+    }
+    this.displayGoodbyeMessage();
+  },
+};
+
+RPSGame.play();
+
+/*
+Notes and reflection
+
+I'm currently contemplating where I could put a potential 'score' state variable, or object. My initial instinct is to 
+put the score and associate it with the player. Add an attribute to the player. Then check the score of all players 
+to see which of the players has at least 5 points, at which point, that player will be declared a winer. The 
+function which checks to see if there is a winner will check to see if the score on any player is a winner. There will
+be two nested loops in the 'play' function, one for the playagain, and one for the best of 5. There will also need 
+to be a new function, displayGrandWinner. Lets update the control flow first and see if there are any clues as to what other options
+I could use for a score. 
+
+Options so far:
+
+- Score on the player object, with a separate function on the RPSGame
+- Score as an object, that contained the two players scores, and a function to check whether one of the scores has reached
+5 and which one. 
+
+*/
