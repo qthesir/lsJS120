cf1880178b65a1434a3415a84d9527b1706962c5
q_the_sir <quinlan.anderson@gmail.com>
2025-11-11 21:24:13 -0500

    problem 1 completed:

---
+++ lesson2-funcs-prototypes/practice-problems-hardbinding/problem1.js
@@ -0,0 +1,8 @@
+/*
+What method can we use to bind a function permanently to a particular execution context?
+*/
+
+/*
+The .bind built-in function method can be used to permanently bind a function to a particular
+execution context.
+*/
