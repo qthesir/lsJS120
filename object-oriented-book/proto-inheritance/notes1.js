9a9db4bd76be91ae385aa8a8cf2c1afe97129681
q_the_sir <quinlan.anderson@gmail.com>
2025-11-30 11:07:20 -0500

    notes

--- object-oriented-book/proto-inheritance/notes1.js
+++ object-oriented-book/proto-inheritance/notes1.js
@@ -1,5 +1,16 @@
-class Cat {
+class Animal {
+  constructor(type) {
+    this.type = type;
+  }
+
+  eat() {
+    console.log("I am eating");
+  }
+}
+
+class Cat extends Animal {
   constructor(name, color) {
+    super();
     this.name = name;
     this.color = color;
   }
@@ -15,4 +26,19 @@ let cheddar = new Cat("Cheddar", "Ginger");
 cheddar.whoAmI();
 
 let cheddarProto = Object.getPrototypeOf(cheddar);
+let cheddarProto2 = Object.getPrototypeOf(cheddarProto);
 console.log(Object.getOwnPropertyNames(cheddarProto));
+console.log(Object.getOwnPropertyNames(cheddarProto2));
+console.log(Object.getPrototypeOf(Cat));
+console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(Cat)));
+console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(Animal)));
+console.log(Object.getOwnPropertyNames(Object.prototype));
+console.log(
+  Object.getPrototypeOf(Object.getPrototypeOf(Animal)) === Object.prototype
+);
+console.log(Cat.prototype instanceof Animal);
+
+// The class prototypes (not Cat.prototype, but Cat.getPrototypeOf) seem to inherit from the class
+// constructor itself, whereas the Cat.prototype is the chain of whats specifically inherited by the
+// instance, although the prototype of "Cat" in the above scenario seems to be of type Animal. Yes...
+// Thats correct. The object prototype on the instance Cheddar is, in fact, an instanceof Animal.
