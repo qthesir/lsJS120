e779d6dfefcf38c8e43d3886b64297ee5fc80cdb
q_the_sir <quinlan.anderson@gmail.com>
2025-12-12 17:24:05 -0500

    notes

--- object-oriented-book/proto-inheritance/notes3.js
+++ object-oriented-book/proto-inheritance/notes3.js
@@ -9,11 +9,12 @@ Animal.prototype.eat = function () {
 function Mammal(name, hasFur) {
   // extends Animal
   // super(name) - we need to simulate super()
-  Animal.call(this, name)
+  Animal.call(this, name);
   this.hasFur = hasFur;
 }
 
 Mammal.prototype = Object.create(Animal.prototype);
+Mammal.prototype.constructor = Mammal;
 
 Mammal.prototype.sleep = function () {
   console.log(`${this.name} is sleeping.`);
@@ -22,11 +23,12 @@ Mammal.prototype.sleep = function () {
 function Dog(name, hasFur, breed) {
   // extends Mammal
   // super(name, hasFur) - we need to simulate super()
-  Mammal.call(this, name, hasFur)
+  Mammal.call(this, name, hasFur);
   this.breed = breed;
 }
 
 Dog.prototype = Object.create(Mammal.prototype);
+Dog.prototype.constructor = Dog;
 
 Dog.prototype.bark = function () {
   console.log(`${this.name} the ${this.breed} is barking.`);
@@ -36,6 +38,11 @@ let myDog = new Dog("Rex", true, "German Shepherd");
 myDog.eat(); // Rex is eating.
 myDog.sleep(); // Rex is sleeping.
 myDog.bark(); // Rex the German Shepherd is barking.
+
+console.log(myDog instanceof Dog);
+for (let i in myDog) {
+  console.log(i);
+}
 
 /*
 When you use the call method, which is on the function prototype, you're explicitly passing in an execution context.
