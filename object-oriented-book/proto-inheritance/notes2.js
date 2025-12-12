40b8c65df568e93642a2f7d56fcd78d1dfaf8176
q_the_sir <quinlan.anderson@gmail.com>
2025-12-12 16:55:42 -0500

    notes

--- object-oriented-book/proto-inheritance/notes2.js
+++ object-oriented-book/proto-inheritance/notes2.js
@@ -17,3 +17,18 @@ catProto = Object.getPrototypeOf(catInstance);
 
 console.log(catProto === Cat.prototype); // False
 console.log(catProto === myProto); // True
+
+console.log(Object.getOwnPropertyNames(Function.prototype));
+console.log(Object.getPrototypeOf(Animal) === Function.prototype);
+console.log(Object.getPrototypeOf(Cat) === Animal);
+
+// Interesting.... Arrow functions do not have function prototypes. They inherity 
+// from the object prototype.
+
+/*
+Clarification: As we've seen, Object.getPrototypeOf returns the object prototype of an object. 
+In this case, the object is a function, and the function's object prototype is Function.prototype. 
+That happens to be a function prototype, but it is not the function prototype for foo.
+ The function prototype, which would be referenced by foo.prototype if it existed, 
+ is undefined since foo doesn't have a prototype property.
+*/
