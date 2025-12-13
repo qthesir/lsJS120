644b8725d15befd468ea934eaf380005a4102f55
q_the_sir <quinlan.anderson@gmail.com>
2025-12-13 08:27:28 -0500

    refactor

--- object-oriented-book/proto-inheritance/exercise2.js
+++ object-oriented-book/proto-inheritance/exercise2.js
@@ -7,35 +7,44 @@ specific kinds of vehicles. All vehicles should be able to accelerate and decele
 boats should be able to drop anchor, and planes should be able to take off and land. Test your code.
 */
 
-function Vehicle(name) {
-  this.name = name;
+function Vehicle(color, weight) {
+  this.color = color;
+  this.weight = weight;
 }
 
 Vehicle.prototype.accelerate = function () {
-  return `This ${this.name} ${this.constructor.name} is Accelerating`;
+  return `This ${this.weight} lb ${this.constructor.name} is Accelerating`;
 };
 
 Vehicle.prototype.decelerate = function () {
-  return `This ${this.name} ${this.constructor.name} is Decelerating`;
+  return `This ${this.weight} lb ${this.constructor.name} is Decelerating`;
 };
 
-function Car(name) {
-  Vehicle.call(this, name);
+function Car(color, weight, licenseNumber) {
+  Vehicle.call(this, color, weight);
+  this.licenseNumber = licenseNumber;
 }
+
 Car.prototype = Object.create(Vehicle.prototype);
 Car.prototype.constructor = Car;
 Car.prototype.honk = function () {
   return `Beep beep`;
 };
 
-function Boat() {}
+function Boat(color, weight, homePort) {
+  Vehicle.call(this, color, weight);
+  homePort = homePort;
+}
 Boat.prototype = Object.create(Vehicle.prototype);
 Boat.prototype.constructor = Boat;
 Boat.prototype.dropAnchor = function () {
   return `Dropping anchor`;
 };
 
-function Plane() {}
+function Plane(color, weight, airline) {
+  Vehicle.call(this, color, weight);
+  this.airline = airline;
+}
 Plane.prototype = Object.create(Vehicle.prototype);
 Plane.prototype.constructor = Plane;
 Plane.prototype.takeOff = function () {
@@ -45,13 +54,15 @@ Plane.prototype.land = function () {
   return `Landing`;
 };
 
-let car = new Car("Jerry");
-let boat = new Boat();
-let plane = new Plane();
+let car = new Car("Red", 3300, "BOXEATR");
+let boat = new Boat("Blue", 14000, "Boston");
+let plane = new Plane("Silver", 35000, "Delta");
 
 console.log(car.accelerate());
 console.log(car.decelerate());
 console.log(car.honk());
+console.log(boat.color);
 console.log(boat.dropAnchor());
+console.log(plane.color);
 console.log(plane.takeOff());
 console.log(plane.land());
