a169c8f7f2c0ac17d1d4c5fd687798a84a15004b
q_the_sir <quinlan.anderson@gmail.com>
2025-12-13 09:10:48 -0500

    practice problem 3 wip

--- lesson3-object-creation-patterns/practice-problems.js
+++ lesson3-object-creation-patterns/practice-problems.js
@@ -8,7 +8,7 @@ parent / child relationships.
 */
 
 /*
-Rewrite the following code to use object-literal syntax to generate the returned object:
+2. Rewrite the following code to use object-literal syntax to generate the returned object:
 */
 
 function makeObj() {
@@ -24,3 +24,34 @@ function makeObj2() {
     propB: 20,
   };
 }
+
+/*
+3. In this problem and the remaining problems, we'll build a simple invoice processing program. 
+To get you started, here's the code to process a single invoice:
+
+To process multiple invoices, we need a factory method that we can use to create invoices. 
+The requirements for the factory function are as follows:
+
+- It returns an invoice object, with phone and internet properties, and a total method.
+- The default value for the phone service is 3000, and the internet service is 5500 (in cents, of course!).
+- The function takes an object argument whose attributes override the default values.
+*/
+
+function createInvoice(phone = 3000, internet = 5500) {
+  return {
+    phone,
+    internet,
+
+    total() {
+      return phone + internet;
+    },
+  };
+}
+
+function invoiceTotal(invoices) {
+  
+}
+
+let invoice = createInvoice();
+
+console.log(invoice.total());
