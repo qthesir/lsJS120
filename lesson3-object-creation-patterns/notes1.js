7b0511b7edc03336dfa73fb01f4e3e896f88f6b2
q_the_sir <quinlan.anderson@gmail.com>
2025-12-17 10:23:32 -0500

    notes

--- lesson3-object-creation-patterns/notes1.js
+++ lesson3-object-creation-patterns/notes1.js
@@ -9,3 +9,21 @@ function Car(make, model, year) {
 let car = new Car("honda", "accord", "2015");
 
 console.log(Car());
+
+let civicArgs = {
+  make: "Honda",
+  model: "Civic",
+  year: "2015",
+  started: false,
+};
+
+function Car2(args) {
+  Object.assign(this, args);
+  this.drive = function () {
+    this.started = true;
+  };
+}
+
+let car2 = new Car2(civicArgs);
+
+console.log(car2);
