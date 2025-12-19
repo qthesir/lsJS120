ae6b8b2219ad3ffbbb6eb92cc0cd4246c513f623
q_the_sir <quinlan.anderson@gmail.com>
2025-12-18 19:33:46 -0500

    practice problem 3 complete

--- lesson3-object-creation-patterns/constructor-practice-problems.js
+++ lesson3-object-creation-patterns/constructor-practice-problems.js
@@ -8,14 +8,14 @@ Consturctor functions use PascalCase instead of camelCase. That is, the first le
 2. What happens if you run the following code? Why? 
 */
 
-function Lizard() {
-  this.scamper = function() {
-    console.log("I'm scampering!");
-  };
-}
+// function Lizard() {
+//   this.scamper = function() {
+//     console.log("I'm scampering!");
+//   };
+// }
 
-let lizzy = Lizard();
-lizzy.scamper(); // ?
+// let lizzy = Lizard();
+// lizzy.scamper(); // ?
 
 /*
 If the following code is run without strict mode, it will return a TypeError. On line 17, Lizard is
@@ -26,3 +26,16 @@ because lizzy is undefined.
 In strict mode, the program would throw a TypeError on line 17, because 'this' inside the function 
 Lizard would be set to undefined instead of the global object. 
 */
+
+/*
+3. Alter the code above so that it produces the desired output
+*/
+
+function Lizard() {
+  this.scamper = function() {
+    console.log("I'm scampering!");
+  };
+}
+
+let lizzy = new Lizard();
+lizzy.scamper(); // ?
