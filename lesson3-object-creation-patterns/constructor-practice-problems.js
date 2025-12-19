858134eed50c78f8dce85a00473159e0a2f8a5be
q_the_sir <quinlan.anderson@gmail.com>
2025-12-18 19:20:32 -0500

    practice problem 2 complete

---
+++ lesson3-object-creation-patterns/constructor-practice-problems.js
@@ -0,0 +1,28 @@
+/*
+1. What naming convention separates constructor functions from other functions?
+
+Consturctor functions use PascalCase instead of camelCase. That is, the first letter is capitalized.
+*/
+
+/*
+2. What happens if you run the following code? Why? 
+*/
+
+function Lizard() {
+  this.scamper = function() {
+    console.log("I'm scampering!");
+  };
+}
+
+let lizzy = Lizard();
+lizzy.scamper(); // ?
+
+/*
+If the following code is run without strict mode, it will return a TypeError. On line 17, Lizard is
+called without the new keyword, and will return undefined, which is assigned to the variable lizzy. 
+On line 18, when the .scamper() method is called on lizzy, the program will throw a TypeError 
+because lizzy is undefined. 
+
+In strict mode, the program would throw a TypeError on line 17, because 'this' inside the function 
+Lizard would be set to undefined instead of the global object. 
+*/
