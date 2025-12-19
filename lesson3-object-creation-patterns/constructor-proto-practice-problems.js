3ce789a1d66f8a25da5c2aff5d39f081a8845ea3
q_the_sir <quinlan.anderson@gmail.com>
2025-12-19 14:28:37 -0500

    problem 2 complete

--- lesson3-object-creation-patterns/constructor-proto-practice-problems.js
+++ lesson3-object-creation-patterns/constructor-proto-practice-problems.js
@@ -3,11 +3,45 @@
 Can you explain why the code produces the output it does?
 */
 
+// let RECTANGLE = {
+//   area: function () {
+//     return this.width * this.height;
+//   },
+//   perimeter: function () {
+//     return 2 * (this.width + this.height);
+//   },
+// };
+
+// function Rectangle(width, height) {
+//   this.width = width;
+//   this.height = height;
+//   this.area = RECTANGLE.area();
+//   this.perimeter = RECTANGLE.perimeter();
+// }
+
+// let rect1 = new Rectangle(2, 3);
+
+// console.log(rect1.area);
+// console.log(rect1.perimeter);
+
+/*
+The above code is going to log NaN in both line 24 and line 25. Inside the Rectangle constructor function, when the 
+property this.area and this.perimeter are set on line 18 and line 19, respectively, the method .area and .perimeter
+are called on the RECTANGLE object, which means that the function is run with RECTANGLE as its execution context. 
+Therefore, this inside of perimeter and area will be the RECTANGLE object which do not have width or height properties, 
+so this.width and this.height will both evaluate to undefined. When the mathematical operator + or * evaluates with 
+undefined or another non-numerical data type as one of its operands, it evaluates to NaN. This is why 24 and 25 evaluate to NaN. 
+*/
+
+/*
+2. How would you fix the code in problem 1?
+*/
+
 let RECTANGLE = {
-  area: function() {
+  area: function () {
     return this.width * this.height;
   },
-  perimeter: function() {
+  perimeter: function () {
     return 2 * (this.width + this.height);
   },
 };
@@ -15,8 +49,8 @@ let RECTANGLE = {
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  this.area = RECTANGLE.area();
-  this.perimeter = RECTANGLE.perimeter();
+  this.area = RECTANGLE.area.call(this);
+  this.perimeter = RECTANGLE.perimeter.call(this);
 }
 
 let rect1 = new Rectangle(2, 3);
@@ -25,11 +59,6 @@ console.log(rect1.area);
 console.log(rect1.perimeter);
 
 /*
-The above code is going to log NaN in both line 24 and line 25. Inside the Rectangle constructor function, when the 
-property this.area and this.perimeter are set on line 18 and line 19, respectively, the method .area and .perimeter
-are called on the RECTANGLE object, which means that the function is run with RECTANGLE as its execution context. 
-Therefore, this inside of perimeter and area will be the RECTANGLE object which do not have width or height properties, 
-so this.width and this.height will both evaluate to undefined. When the operand + or * encounters undefined or another 
-non-numerical data type, it evaluates to NaN. This is why 24 and 25 evaluate to NaN. 
+Simplest solution is to use .call to pass the current objects this to the execution context. You could also 
+accept width and height as parameters to the functions, but I think .call is cleaner. 
 */
-
