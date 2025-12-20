c8aadd2121e42c09ab01c6f157639ffcde1ccc46
q_the_sir <quinlan.anderson@gmail.com>
2025-12-19 21:53:11 -0500

    problem 3 complete

--- lesson3-object-creation-patterns/constructor-proto-practice-problems.js
+++ lesson3-object-creation-patterns/constructor-proto-practice-problems.js
@@ -62,3 +62,24 @@ console.log(rect1.perimeter);
 Simplest solution is to use .call to pass the current objects this to the execution context. You could also 
 accept width and height as parameters to the functions, but I think .call is cleaner. 
 */
+
+/*
+3. Write a constructor function called Circle that takes a radius as an argument. You should be 
+able to call an area method on any objects created by the constructor to get the circle's area. 
+Test your implementation with the following code:
+*/
+
+function Circle(radius) {
+  this.radius = radius;
+}
+
+Circle.prototype.area = function () {
+  return this.radius ** 2 * Math.PI;
+};
+
+let a = new Circle(3);
+let b = new Circle(4);
+
+console.log(a.area().toFixed(2)); // => 28.27
+console.log(b.area().toFixed(2)); // => 50.27
+console.log(a.hasOwnProperty("area")); // => false
