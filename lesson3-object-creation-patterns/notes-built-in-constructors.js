9813252c6e8e502f3bca80b3f5e208099959579c
q_the_sir <quinlan.anderson@gmail.com>
2025-12-22 16:43:44 -0500

    notes

---
+++ lesson3-object-creation-patterns/notes-built-in-constructors.js
@@ -0,0 +1,12 @@
+let array = new Array(3);
+let array2 = new Array(3).fill(undefined);
+
+array.forEach((ele) => console.log(ele));
+
+array2.forEach((ele) => console.log(ele));
+
+console.log(array[0]);
+console.log(array[1]);
+
+console.log(array.length)
+console.log(array.length)
