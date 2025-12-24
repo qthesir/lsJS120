dab01e3f302cb22d754be088ea3cb459414abc1c
q_the_sir <quinlan.anderson@gmail.com>
2025-12-24 12:00:37 -0500

    problem 1 complete

---
+++ lesson3-object-creation-patterns/classes-practice-problems.js
@@ -0,0 +1,10 @@
+/*
+1. What do we mean when we say that classes are first-class values?
+*/
+
+/*
+Classes are first class values because they are constructor functions, which are functions, which are objects. In JavaScript, 
+functions can be taken as parameters to other functions and also be return values. They can also be assigned to variables, 
+and used anywhere where a value is expected. First-class values means that they are able to be used as you would any other 
+JavaScript value.
+*/
