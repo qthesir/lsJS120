2d0f7f844dfc077e26bdaf422131608281bea034
q_the_sir <quinlan.anderson@gmail.com>
2025-12-25 09:18:26 -0500

    problems 1 and 2 complete: :

--- lesson3-object-creation-patterns/classes-practice-problems.js
+++ lesson3-object-creation-patterns/classes-practice-problems.js
@@ -8,3 +8,28 @@ functions can be taken as parameters to other functions and also be return value
 and used anywhere where a value is expected. First-class values means that they are able to be used as you would any other 
 JavaScript value.
 */
+
+/*
+2. Consider the following class declaration: 
+*/
+
+class Television {
+  static manufacturer() {
+    // omitted code
+  }
+
+  model() {
+    // method logic
+  }
+}
+
+console.log(Object.getOwnPropertyNames(Television));
+console.log(Object.getOwnPropertyNames(Television.prototype));
+
+/*
+What does the static modifier do? How would we call the method manufacturer?
+
+A static method is a method that lives on the constructor function. That is, when the class is defined, 
+the manufacturer method is an own property of the constructor. Manufacturer would be accessed by callint
+Television.manufacturer().
+*/
