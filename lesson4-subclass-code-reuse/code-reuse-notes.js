55350aad67decc5bf709a23b08f6b9b2b39c74b3
q_the_sir <quinlan.anderson@gmail.com>
2026-01-15 10:52:25 -0500

    notes

---
+++ lesson4-subclass-code-reuse/code-reuse-notes.js
@@ -0,0 +1,56 @@
+/* 
+Orchestrate a wedding that includes Chefs, decoradors, and musicians. There should be a
+function on wedding that tells all of those entities to prepare their respective roles 
+for the celebration. Don't worry about the implementation - just the OOO structure. 
+*/
+
+class Chef {
+  prepare(wedding) {
+    this.prepareFood(wedding.food);
+  }
+
+  prepareFood(food) {
+    console.log(`Preparing ${food}`);
+  }
+}
+
+class Musician {
+  prepare(wedding) {
+    this.prepareMusic(wedding.songs);
+  }
+  prepareMusic(songs) {
+    console.log(`Preparing ${songs}`);
+  }
+}
+
+class Decorator {
+  prepare(wedding) {
+    this.prepareDecorations(wedding.decorations);
+  }
+
+  prepareDecorations(decorations) {
+    console.log(`Preparing ${decorations}`);
+  }
+}
+
+class Wedding {
+  constructor(food, songs, decorations) {
+    this.food = food;
+    this.songs = songs;
+    this.decorations = decorations;
+  }
+
+  preparations(preparers) {
+    preparers.forEach((preparer) => preparer.prepare(this));
+  }
+}
+
+let chef = new Chef();
+let musician = new Musician();
+let decorator = new Decorator();
+let wedding = new Wedding('chicken', 'piano', 'balloons'); 
+ 
+let preparers = [chef, musician, decorator];
+
+wedding.preparations(preparers);
+
