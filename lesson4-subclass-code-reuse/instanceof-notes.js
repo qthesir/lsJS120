3acc75ea7857f93481212314fdeccafc0eaa068b
q_the_sir <quinlan.anderson@gmail.com>
2026-01-17 18:08:04 -0500

    notes

--- lesson4-subclass-code-reuse/instanceof-notes.js
+++ lesson4-subclass-code-reuse/instanceof-notes.js
@@ -12,5 +12,6 @@ console.log(cat instanceof Animal)
 /*
 Ok, I think I understand this now. Instanceof is checking to see if any of the objects in the instances 
 (left hand operand) prototype chain are equal to the object reference of the constructor's (right hand operand)
-function prototype (constructor.prototype). 
+function prototype (constructor.prototype). This now makes perfect sense. And writing Object.create(Animal.prototype)
+is kinda equivalent to new Animal(). I did not think that one through hard enough. 
 */
