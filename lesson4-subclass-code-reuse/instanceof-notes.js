b41b43b2dcc677679b33de6203661f973f42d9ad
q_the_sir <quinlan.anderson@gmail.com>
2026-01-17 18:07:05 -0500

    notes on instanceof

---
+++ lesson4-subclass-code-reuse/instanceof-notes.js
@@ -0,0 +1,16 @@
+function Animal() {}
+
+function Cat() {}
+
+Cat.prototype = Object.create(Animal.prototype)
+
+let cat = new Cat()
+
+console.log(Animal.prototype instanceof Animal)
+console.log(cat instanceof Animal)
+
+/*
+Ok, I think I understand this now. Instanceof is checking to see if any of the objects in the instances 
+(left hand operand) prototype chain are equal to the object reference of the constructor's (right hand operand)
+function prototype (constructor.prototype). 
+*/
