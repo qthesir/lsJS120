06548a322dc0c00da1f58db40ab4ab6feab7723d
q_the_sir <quinlan.anderson@gmail.com>
2026-04-04 17:02:48 -0400

    wip

--- exercises/objects/school.js
+++ exercises/objects/school.js
@@ -40,40 +40,6 @@ function createSchool() {
       });
     },
 
-    /* principle issue with this one is that there is a list of unique courses. Maybe I create a new object, which contains the course
-      and all the grades, and then iterates through the list of grades and set a variable course name. But this feels kind wrong, 
-      because I'm setting the variable a bunch of times. And then what to do if I can't find a grade? I also need the course name 
-      before I start logging the individual grades and then the average. So there's a few things going on in this function. First thing
-      is that I need to render the name. then i need to render the individual grades... fuck, with the students name. and then I need to average all the grades. So I need
-      to get the list of courses first, perhaps, and then extract the information from that list. So I need to create a list of all the
-      courses. I need to use that list to get the name, log the name from the first course in the list. Then I can iterate through the 
-      list of courses and log the grade. then I can calculate the average grade and log the average. Thats it. Got it. 
-      
-      Nope, don't quite got it. I think I need to take a step back and do this out in pseudocode. 
-
-      Input: courseCode
-      Output: Course name headline, list of name of student with their corresponding grade for the course, and then the average 
-      grade of all students. 
-
-      I almost need a need data structure. I could create a new list, which contains objects with 3 properties: nameOfCourse, nameOfStudent,
-      and grade. That gives me everything I need to render the course info. So why dont I do that? Oh, but for the average, I still have 
-      to filter the list of grades to just the students that actually have a grade. if there is no grade, then it is 0. 
-
-      Algorithm 
-      - Accept course code as an input
-      - Declare a variable "courseList"
-      - Iterate through the list of students
-      - If the student has the course that matches the code:
-        - Create a new object with student name from the student, the course name, and the grade
-        - add the object to course list
-      - Log the course header with the course name from the first element of the courseList array
-      - For each element in the course list:
-        - Log the name of the student and the grade
-      - Compute the average grade of all the students from the course list
-      - Log the average grade. 
-
-      */
-
     courseReport(courseCode) {
       let courseList = [];
       this.students.forEach((student) => {
@@ -208,3 +174,41 @@ school.courseReport(102);
 school.courseReport(202);
 
 school.addStudent("Bar", "6th");
+
+    /* 
+      
+      PEDAC for school.courseReport() 
+      
+      principle issue with this one is that there is a list of unique courses. Maybe I create a new object, which contains the course
+      and all the grades, and then iterates through the list of grades and set a variable course name. But this feels kind wrong, 
+      because I'm setting the variable a bunch of times. And then what to do if I can't find a grade? I also need the course name 
+      before I start logging the individual grades and then the average. So there's a few things going on in this function. First thing
+      is that I need to render the name. then i need to render the individual grades... fuck, with the students name. and then I need to average all the grades. So I need
+      to get the list of courses first, perhaps, and then extract the information from that list. So I need to create a list of all the
+      courses. I need to use that list to get the name, log the name from the first course in the list. Then I can iterate through the 
+      list of courses and log the grade. then I can calculate the average grade and log the average. Thats it. Got it. 
+      
+      Nope, don't quite got it. I think I need to take a step back and do this out in pseudocode. 
+
+      Input: courseCode
+      Output: Course name headline, list of name of student with their corresponding grade for the course, and then the average 
+      grade of all students. 
+
+      I almost need a need data structure. I could create a new list, which contains objects with 3 properties: nameOfCourse, nameOfStudent,
+      and grade. That gives me everything I need to render the course info. So why dont I do that? Oh, but for the average, I still have 
+      to filter the list of grades to just the students that actually have a grade. if there is no grade, then it is 0. 
+
+      Algorithm 
+      - Accept course code as an input
+      - Declare a variable "courseList"
+      - Iterate through the list of students
+      - If the student has the course that matches the code:
+        - Create a new object with student name from the student, the course name, and the grade
+        - add the object to course list
+      - Log the course header with the course name from the first element of the courseList array
+      - For each element in the course list:
+        - Log the name of the student and the grade
+      - Compute the average grade of all the students from the course list
+      - Log the average grade. 
+
+      */
