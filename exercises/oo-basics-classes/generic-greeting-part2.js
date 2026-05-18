6dbfe76e56bd050338bcfd2d557bdcf9f6c49e1e
q_the_sir <quinlan.anderson@gmail.com>
2026-05-18 17:53:03 -0400

    OO basics classes complete

---
+++ exercises/oo-basics-classes/generic-greeting-part2.js
@@ -0,0 +1,17 @@
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  static genericGreeting() {
+    console.log("Hello! I am a cat!");
+  }
+
+  personalGreeting() {
+    console.log(`Hello! My name is ${this.name}!`);
+  }
+}
+
+let kitty = new Cat("Sophie");
+Cat.genericGreeting();
+kitty.personalGreeting();
